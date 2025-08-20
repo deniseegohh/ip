cd79449ba8b99e85460693caab4ca9a83140e63d
deniseegohh <e1156128@u.nus.edu>
2025-08-20 23:56:15 +0800

    echos commands entered by the user, and exits when the user types the command bye

--- src/main/java/Pepero.java
+++ src/main/java/Pepero.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Pepero {
     public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Pepero! \nWhat can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye bye!~ Hope to see you again soon~");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye bye!~ Hope to see you again soon~");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        sc.close();
     }
 }
