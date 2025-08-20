a7857b61c2a53bd87efa3a422bb7c0d527d06538
deniseegohh <e1156128@u.nus.edu>
2025-08-20 23:35:52 +0800

    Renamed Chatbot, implemented initial skeletal version

---
+++ src/main/java/Pepero.java
@@ -0,0 +1,9 @@
+public class Pepero {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Pepero! \nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye bye!~ Hope to see you again soon~");
+        System.out.println("____________________________________________________________");
+    }
+}
