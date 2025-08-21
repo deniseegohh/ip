6271dcabbdfdb1c410733d4f5f57264dd7395050
deniseegohh <e1156128@u.nus.edu>
2025-08-21 14:49:12 +0800

    added ability to store whatever text entered by the user and display back to user when requested

--- src/main/java/Pepero.java
+++ src/main/java/Pepero.java
@@ -1,23 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Pepero {
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Task> tasks = new ArrayList<>();
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Pepero! \nWhat can I do for you?");
         System.out.println("____________________________________________________________");
 
         while (true) {
             String input = sc.nextLine();
+            System.out.println("____________________________________________________________");
+
             if (input.equals("bye")) {
                 System.out.println("Bye bye!~ Hope to see you again soon~");
                 System.out.println("____________________________________________________________");
                 break;
+
+            } else if (input.equals("list")) {
+                for (int i = 1; i <= tasks.size(); i++) {
+                    System.out.println(i + ". " + tasks.get(i - 1).getDescription());
+                }
+                System.out.println("____________________________________________________________");
+
             } else {
-                System.out.println(input);
+                tasks.add(new Task(input));
+                System.out.println("added: " + input);
                 System.out.println("____________________________________________________________");
             }
         }
+
         sc.close();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+
+    private final String description;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
