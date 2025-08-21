9d2d541f5dac32bc77f1162989ecd7423eaea2c4
deniseegohh <e1156128@u.nus.edu>
2025-08-21 15:08:50 +0800

    added ability to mark tasks as done or undone

--- src/main/java/Pepero.java
+++ src/main/java/Pepero.java
@@ -12,19 +12,35 @@ public class Pepero {
 
         while (true) {
             String input = sc.nextLine();
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
             System.out.println("____________________________________________________________");
 
-            if (input.equals("bye")) {
+            if (command.equals("bye")) {
                 System.out.println("Bye bye!~ Hope to see you again soon~");
                 System.out.println("____________________________________________________________");
                 break;
 
-            } else if (input.equals("list")) {
+            } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= tasks.size(); i++) {
-                    System.out.println(i + ". " + tasks.get(i - 1).getDescription());
+                    System.out.println(i + "." + tasks.get(i - 1).getStatusIcon() + " " + tasks.get(i - 1).getDescription());
                 }
                 System.out.println("____________________________________________________________");
-
+            } else if (command.equals("mark")) {
+                int number = Integer.parseInt(parts[1]);
+                Task task = tasks.get(number - 1);
+                task.markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task.getStatusIcon() + " " + task.getDescription());
+                System.out.println("____________________________________________________________");
+            } else if (command.equals("unmark")) {
+                int number = Integer.parseInt(parts[1]);
+                Task task = tasks.get(number - 1);
+                task.unmarkDone();
+                System.out.println("Okay, I've marked this task as not done yet:");
+                System.out.println(task.getStatusIcon() + " " + task.getDescription());
+                System.out.println("____________________________________________________________");
             } else {
                 tasks.add(new Task(input));
                 System.out.println("added: " + input);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,30 @@
 public class Task {
 
     private final String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void unmarkDone() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        if (isDone) {
+            return("[X]");
+        } else {
+            return("[ ]");
+        }
     }
 }
