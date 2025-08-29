8d29c0b7e2baea2ef13854ecd8175ff9860ccfe4
deniseegohh <e1156128@u.nus.edu>
2025-08-30 02:27:17 +0800

    edit ParserTest

--- src/test/java/pepero/ParserTest.java
+++ src/test/java/pepero/ParserTest.java
@@ -3,9 +3,7 @@ package pepero;
 import org.junit.jupiter.api.Test;
 import pepero.task.TaskList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class ParserTest {
 
