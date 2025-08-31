6d91173342ef5f790d13625eae586fc2b758cd27
deniseegohh <e1156128@u.nus.edu>
2025-08-31 17:35:50 +0800

    Add JavaDoc for Event.java

--- src/main/java/pepero/task/Event.java
+++ src/main/java/pepero/task/Event.java
@@ -3,11 +3,21 @@ package pepero.task;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a task that occurs over a period of time.
+ */
 public class Event extends Task {
 
     protected LocalDateTime from;
     protected LocalDateTime to;
 
+    /**
+     * Constructs a new task with the given description, start time and end time.
+     *
+     * @param description the description of the event
+     * @param from the start date and time of the event
+     * @param to the end date and time of the event
+     */
     public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
@@ -15,6 +25,14 @@ public class Event extends Task {
         taskCount++;
     }
 
+    /**
+     * Constructs a new task with the given description, start time, end time and completion status.
+     *
+     * @param description the description of the event
+     * @param from the start date and time of the event
+     * @param to the end date and time of the event
+     * @param isDone the initial completion status of the event
+     */
     public Event(String description, LocalDateTime from, LocalDateTime to, boolean isDone) {
         super(description);
         this.from = from;
@@ -22,6 +40,12 @@ public class Event extends Task {
         this.isDone = isDone;
     }
 
+    /**
+     * Returns the status icon for the event, showing "[E]" for event
+     * and "X" if done, " " if not done.
+     *
+     * @return a string representing the status of the task
+     */
     @Override
     public String getStatusIcon() {
         if (isDone) {
@@ -31,11 +55,23 @@ public class Event extends Task {
         }
     }
 
+    /**
+     * Returns a string representation of the event for displaying to the user,
+     * including the status icon, description, start time and end time.
+     *
+     * @return a string representing the task
+     */
     @Override
     public String toString() {
         return this.getStatusIcon() + " " + this.getDescription() + " (from: " + from.format(DateTimeFormatter.ofPattern("ddMMyy HHmm")) + " to: " + to.format(DateTimeFormatter.ofPattern("ddMMyy HHmm")) + ")";
     }
 
+    /**
+     * Returns a string representation of the event formatted for saving to a file,
+     * including type, completion status, description, start time and end time.
+     *
+     * @return a string in the file storage format
+     */
     @Override
     public String toFileFormat() {
         return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from.format(DateTimeFormatter.ofPattern("ddMMyy HHmm")) + "-" + to.format(DateTimeFormatter.ofPattern("ddMMyy HHmm"));
